7c43bbe300194c4521e32bce7aaef4bbccb20f33
asmss <139372952+asmss@users.noreply.github.com>
2026-01-12 23:53:04 +0300

    render düzenlemesi

--- backend/config/firestore.js
+++ backend/config/firestore.js
@@ -1,12 +1,20 @@
-const admin = require("firebase-admin")
-const serviceAccount = require("../kimhakli-8f416-firebase-adminsdk-fbsvc-bd0983cb77.json");
+const admin = require("firebase-admin");
 
+let serviceAccount;
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
-});
+if (process.env.FIREBASE_SERVICE_ACCOUNT) {
+  serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+} else {
+  serviceAccount = require("../../kimhakli-8f416-firebase-adminsdk-fbsvc-bd0983cb77.json");
+}
+
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+  });
+}
 
 const db = admin.firestore();
-
+db.settings({ preferRest: true });
 
 module.exports = db;
